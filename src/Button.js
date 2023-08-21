c668936805d0794a44e48fb1bffefcf547766524
LillyIlex <116085080+LillyIlex@users.noreply.github.com>
2023-08-21 15:00:38 +0100

    Added hover on button and made columns responsive

---
+++ src/Button.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { RxLightningBolt } from "react-icons/rx";
+import { BiBrain } from "react-icons/bi";
+import { AiOutlineEye } from "react-icons/ai";
+import { LiaFacebookMessenger } from "react-icons/lia";
+
+function Button(props) {
+  function getColour() {
+    switch (props.title) {
+      case "Reaction":
+        return "text-primary-light-red bg-rose-50";
+      case "Memory":
+        return "text-primary-orangey-yellow bg-amber-50";
+      case "Verbal":
+        return "text-primary-green-teal bg-teal-50";
+      case "Visual":
+        return "text-primary-cobalt-blue bg-indigo-50";
+    }
+  }
+  function getIcon() {
+    switch (props.title) {
+      case "Reaction":
+        return reactionIcon;
+      case "Memory":
+        return memoryIcon;
+      case "Verbal":
+        return verbalIcon;
+      case "Visual":
+        return visualIcon;
+    }
+  }
+
+  const memoryIcon = <BiBrain />;
+  const reactionIcon = <RxLightningBolt />;
+  const visualIcon = <AiOutlineEye />;
+  const verbalIcon = <LiaFacebookMessenger />;
+
+  const colour = getColour();
+  const icon = getIcon();
+
+  return (
+    <button
+      className={`flex justify-between w-64 rounded-md  ${colour} px-6 py-3 mb-3 text-sm font-semibold`}
+    >
+      <a>{icon}</a>
+      {props.title} <a> </a>
+      <p className="text-gray-400 ">
+        <a className="text-black">{props.score} </a>/ 100{" "}
+      </p>
+    </button>
+  );
+}
+
+export default Button;
--- src/Results.js
+++ src/Results.js
@@ -1,108 +1,61 @@
 import React from "react";
-import { RxLightningBolt } from "react-icons/rx";
-import { BiBrain } from "react-icons/bi";
-import { AiOutlineEye } from "react-icons/ai";
-import { LiaFacebookMessenger } from "react-icons/lia";
 
+import data from "./data.json";
+import Button from "./Button";
 
-const title = () => {
-  for (i)
-  if (type === reaction) {
-      return "Reaction" 
-  } else if (type === memory) {
-      return "Memory"
-  } else if (type === verbal) {
-      return "Verbal" 
-  } else if (type === visual) {
-      return "Visual"
-  }
-}
-
-<button id="button-title" className="flex justify-between w-64 rounded-md bg-teal-50  text-primary-green-teal px-6 py-3 mb-3 text-sm font-semibold">
-<LiaFacebookMessenger />
-Verbal <a> </a>
-<p className="text-gray-400 ">
-  <a id="score" className="text-black">61</a> / 100
-</p>
-</button>
+console.log(data);
 
 function Results() {
   return (
-    <div className="flex grid-cols-1 md:grid-cols-2 text-white font- place-content-center md:container md:mx-auto">
-      <div>
-        <div className="block max-w-xs h-96 w-72 rounded-2xl bg-indigo-600 bg-gradient-to-b from-gradients-light-slate-blue to-gradients-light-royal-blue p-4 pb-7 text-center shadow-[0_4px_9px_-4px_#3b71ca] my-10 ">
-          <div className=" text-2xl  border-black py-1 px-6 ">
-            <p className="mt-2 text-lg font-semibold leading-7 text-gray-200">Your Result</p>
-            <h3 className="text-6xl font-bold text-white border-0 rounded-full bg-gradient-to-b from-gradients-violet-blue to-gradients-persian-blue w-36 h-36 mt-4 pt-6 ml-8">
-              76
-              <p className="flex-none text-sm font-medium leading-6 text-gray-400 mb-2">
-                {" "}
-                of 100{" "}
+    <>
+      <div className="flex grid-cols-1 md:grid-cols-2 text-white font- place-content-center md:container md:mx-auto">
+        <div>
+          <div className="block max-w-xs h-96 w-72 rounded-2xl bg-indigo-600 bg-gradient-to-b from-gradients-light-slate-blue to-gradients-light-royal-blue p-4 pb-7 text-center shadow-[0_4px_9px_-4px_#3b71ca] my-10 ">
+            <div className=" text-2xl  border-black py-1 px-6 ">
+              <p className="mt-2 text-lg font-semibold leading-7 text-gray-200">
+                Your Result
               </p>
-            </h3>
+              <h3 className="text-6xl font-bold text-white border-0 rounded-full bg-gradient-to-b from-gradients-violet-blue to-gradients-persian-blue w-36 h-36 mt-4 pt-6 ml-8">
+                76
+                <p className="flex-none text-sm font-medium leading-6 text-gray-400 mb-2">
+                  {" "}
+                  of 100{data.map}
+                </p>
+              </h3>
 
-            <p className="mt-4 text-2xl text-white font-semibold ">Great</p>
-            <p className="mt-2 text-sm text-gray-300 ">
-              You scored higher than 65% of these people who have taken these
-              tests.
-            </p>
+              <p className="mt-4 text-2xl text-white font-semibold ">Great</p>
+              <p className="mt-2 text-sm text-gray-300 ">
+                You scored higher than 65% of these people who have taken these
+                tests.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div>
-        <div className="block rounded-2xl h-96 w-72 bg-white text-center shadow-[0_4px_9px_-4px_#3b71ca] my-10">
-          <div className=" text-2xl border-black py-1 px-6 ">
-            <h3 className="text-gray-800 m-6 ml-4 mt-4 text-left text-base font-bold">
-              
-              Summary
-            </h3>
-            <div>
-              <button className="flex justify-between w-64 rounded-md bg-rose-50 text-primary-light-red px-6 py-3 mb-3 text-sm font-semibold">
-                
-                <RxLightningBolt />
-                Reaction <a> </a>
-                <p className="text-gray-400 ">
-                  {" "}
-                  <a className="text-black">80</a> / 100
-                </p>
-              </button>
+        <div>
+          <div className="block rounded-2xl h-96 w-72 bg-white text-center shadow-[0_4px_9px_-4px_#3b71ca] my-10">
+            <div className=" text-2xl border-black py-1 px-6 ">
+              <h3 className="text-gray-800 m-6 ml-4 mt-4 text-left text-base font-bold">
+                Summary
+              </h3>
+              <div>
+                {data.map((item) => (
+                  <Button
+                    title={item.category}
+                    score={item.score}
+                    icon={item.icon}
+                  />
+                ))}
+              </div>
 
-              <button className="flex justify-between w-64 rounded-md bg-amber-50 text-primary-orangey-yellow px-6 py-3 mb-3 text-sm font-semibold">
-              <BiBrain />
-                Memory <a> </a>
-                <p className="text-gray-400 ">
-                  
-                  <a className="text-black">92</a> / 100
-                </p>
-              </button>
-
-              <button className="flex justify-between w-64 rounded-md bg-teal-50  text-primary-green-teal px-6 py-3 mb-3 text-sm font-semibold">
-                <LiaFacebookMessenger />
-                Verbal <a> </a>
-                <p className="text-gray-400 ">
-                  <a className="text-black">61</a> / 100
-                </p>
-              </button>
-
-              <button className="flex justify-between w-64 rounded-md bg-indigo-50 text-primary-cobalt-blue px-6 py-3 mb-3 text-sm font-semibold">
-                <AiOutlineEye />
-                Visual <a> </a>
-                <p className="text-gray-400 ">
-                
-                  <a className="text-black">72</a> / 100
-                </p>
+              <button className="rounded-full  bg-indigo-600 bg-gradient-to-b from-gradients-light-slate-blue to-gradients-light-royal-blue text-base px-4 py-3 mr-4 mt-6 w-60  hover:bg-indigo-800">
+                Continue
               </button>
             </div>
-
-            <button className="rounded-full  bg-indigo-600 bg-gradient-to-b from-gradients-light-slate-blue to-gradients-light-royal-blue text-base px-4 py-3 mr-4 mt-6 w-60  hover:bg-indigo-800">
-              {" "}
-              Continue{" "}
-            </button>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
